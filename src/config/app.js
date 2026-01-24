15ef00f1b63c202032f68e6b71dc82da10501844
Sandeep Bansal <thisbansal@gmail.com>
2026-01-24 16:05:09 +1100

    cleanup

--- src/config/app.js
+++ src/config/app.js
@@ -1,7 +1,7 @@
 export const PLEX_CONFIG = {
-  clientId: '877f312d-6d9b-4877-8698-6894470023fc', // ✅ Fine to commit
+  clientId: '877f312d-6d9b-4877-8698-6894470023fc',
   product: 'Plex for webOS',
   device: 'webOS TV',
   apiUrl: 'https://plex.tv/api/v2',
-  appId: 'com.nookbyte.aresapp.v1'
+  appId: 'com.nookbyte.aresapp.v1',
 };
