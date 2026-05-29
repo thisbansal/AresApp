7ca2a582dd7d13e3c1b04a133499e285ed969eae
Sandeep Bansal <thisbansal@gmail.com>
2026-05-29 11:19:36 +1000

    chore: Rename X-Plex-Product to Ares App

--- src/config/app.js
+++ src/config/app.js
@@ -1,6 +1,6 @@
 export const PLEX_CONFIG = {
   clientId: '877f312d-6d9b-4877-8698-6894470023fc',
-  product: 'Plex for webOS',
+  product: 'Ares App',
   device: 'webOS TV',
   apiUrl: 'https://plex.tv/api/v2',
   appID: 'com.nookbyte.aresapp',
