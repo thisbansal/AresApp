ce83f2f4fe703d42027baff15d1bcf4947eaa879
Sandeep Bansal <thisbansal@gmail.com>
2026-06-20 14:22:50 +1000

    fix: immediately invoke IIFE in ContentBrowserPage render to fix React child error

--- src/routes/ContentBrowserPage.jsx
+++ src/routes/ContentBrowserPage.jsx
@@ -1031,7 +1031,7 @@ function ContentBrowserPage() {
               )}
             </>
             );
-          })}
+          })()}
 
           {activeTab.type === 'library' && (
             <>
